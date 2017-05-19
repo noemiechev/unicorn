db73879da25856fc4faa06e0ec8b1a0f48b0f7c4
Armen <armen.mahmedov@gmail.com>
2017-05-19 17:06:46 +0200

    split info

--- src/epreuveia/EpreuveIA.java
+++ src/epreuveia/EpreuveIA.java
@@ -8,6 +8,8 @@ package epreuveia;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,14 +20,33 @@ public class EpreuveIA {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
-        //InetAddress ip = new InetAddress();
-        client s = new client("127.0.0.1", 1337);
-        s.envoyerNom("DeletePackages");
-        while(1==1){
-            s.recuperation();
-            System.out.println(s.recuperation());
+    public static void main(String[] args) {
+        try {
+            String nomEquipe = "127.0.0.1";
+            String infoServ = "";
+            String numeroJoueur;
+            client s = new client(nomEquipe, 1337);
+            s.envoyerNom("DeletePackages");
+            
+            numeroJoueur = s.recuperation();
+
+            while(infoServ != "FIN"){
+                infoServ = s.recuperation();  // recuperer les info du serveur
+                String infos[] =  infoServ.split("/");
+                System.out.println("Numero du joueur : " + numeroJoueur);
+                
+                if (!infoServ.equals("FIN")){
+                    System.out.println("" + infos[0]);
+                    System.out.println("" + infos[1]);
+                    System.out.println("" + infos[2]);
+                }
+                // System.out.println(infoServ);
+                
+            }
+        } catch (IOException ex) {
+            System.out.println("Connexion au serveur refuséee !!!!!! Lance le serveur ");
         }
+        
     }
     
 }
